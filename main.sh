383304c2c98ea31db643770d4ba8ec7093027b2e
si-yuan20 <82247950+si-yuan20@users.noreply.github.com>
2026-04-10 18:18:56 +0800

    Add seed parameter to training commands in main.sh

--- main.sh
+++ main.sh
@@ -1,20 +1,20 @@
 
 echo "Starting experiment with PolypSegNet_Full model..."
-python main.py --mode train --dataset cvc-clinicdb --model PolypSegNet_Full
+python main.py --mode train --dataset cvc-clinicdb --model PolypSegNet_Full --seed 2024
 
 echo "Starting experiment with PolypSegNet_Resnet_SCAS model..."
-python main.py --mode train --dataset cvc-clinicdb --model PolypSegNet_Resnet_SCAS
+python main.py --mode train --dataset cvc-clinicdb --model PolypSegNet_Resnet_SCAS --seed 2024
 
 echo "Starting experiment with PolypSegNet_Resnet_D2F model..."
-python main.py --mode train --dataset cvc-clinicdb --model PolypSegNet_Resnet_D2F
+python main.py --mode train --dataset cvc-clinicdb --model PolypSegNet_Resnet_D2F  --seed 2024
 
 echo "Starting experiment with PolypSegNet_Resnet model..."
-python main.py --mode train --dataset cvc-clinicdb --model PolypSegNet_Resnet
+python main.py --mode train --dataset cvc-clinicdb --model PolypSegNet_Resnet  --seed 2024
 
 echo "Starting experiment with PolypSegNet_D2F model..."
-python main.py --mode train --dataset cvc-clinicdb --model PolypSegNet_D2F
+python main.py --mode train --dataset cvc-clinicdb --model PolypSegNet_D2F  --seed 2024
 
  echo "Starting experiment with PolypSegNet_Baseline model..."
- python main.py --mode train --dataset cvc-clinicdb --model PolypSegNet_Baseline
+ python main.py --mode train --dataset cvc-clinicdb --model PolypSegNet_Baseline  --seed 2024
 
 echo "experiment completed."
